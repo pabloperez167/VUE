c8f15047351252e33d0ea30d8d63065fb5b1de18
Pablo <pplimonera@gmail.com>
2023-05-17 18:35:43 +0200

    subida

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Consumir API con Vue</h1>
+    <api-component></api-component>
+  </div>
+</template>
+
+<script>
+import ApiComponent from './ApiComponent.vue';
+
+export default {
+  components: {
+    ApiComponent, 
+  },
+};
+</script>
---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+
+
+const routes = [{
+    path: '/http://127.0.0.1:8000/api/Starship',
+    name: 'Starship'
+    
+}
+
+
+]
